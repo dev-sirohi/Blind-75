27a020dc428a5f215a7f01f856ac723daa17ec09
Dev Sirohi <74674617+dev-sirohi@users.noreply.github.com>
2023-04-01 08:16:54 +0530

    Update LRUCache.java

--- Day 52/LRUCache.java
+++ Day 52/LRUCache.java
@@ -1,3 +1,5 @@
+// THIS IS THE CODE I EDITED FOR OVER 2 HOURS AFTER COPYING FROM SOLUTION
+
 class LRUCache {
     class DLL {
         int key, val;
@@ -76,9 +78,159 @@ class LRUCache {
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+// THIS IS THE CODE THAT I WROTE EARLIER
+
+class LRUCache {
+    class DLL {
+        int key, val;
+        DLL pre, post;
+    }
+
+    void add(DLL node) {
+        node.pre = head;
+        node.post = head.post;
+
+        head.post = node;
+        head.post.pre = node;
+    }
+
+    void remove(DLL node) {
+        node.pre.post = node.post;
+        node.post.pre = node.pre;
+    }
+
+    HashMap<Integer, DLL> cache;
+    int capacity;
+    int count;
+    DLL head, tail;
+
+    // THIS IS WHERE THE MAIN CODE STARTS
+
+    public LRUCache(int capacity) {
+        cache = new HashMap<Integer, DLL>();
+        this.capacity = capacity;
+        count = 0;
+
+        head = new DLL();
+        tail = new DLL();
+        head.pre = null;
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+    
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+            add(cache.get(key));
+            return cache.get(key).val;
+        }
+
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (!cache.containsKey(key)) {
+            DLL node = new DLL();
+            node.key = key;
+            node.val = value;
+            cache.put(key, node);
+            
+            if (count < capacity) {
+                count++;
+                add(node);
+            } else {
+                cache.remove(tail.pre.key);
+                tail.pre.pre.post = tail;
+                tail.pre = tail.pre.pre;
+                add(node);
+            }
+        } else {
+            cache.get(key).val = value;
+            remove(cache.get(key));
+            add(cache.get(key));
+        }
+    }
+}
+
+// THIS IS THE CODE THAT I WROTE EARLIER BUT FIXED
+
+class LRUCache {
+    class DLL {
+        int key, val;
+        DLL pre, post;
+    }
+
+    void add(DLL node) {
+        node.pre = head;
+        node.post = head.post;
+        head.post.pre = node;
+        head.post = node;
+    }
+
+    void remove(DLL node) {
+        node.pre.post = node.post;
+        node.post.pre = node.pre;
+    }
+
+    HashMap<Integer, DLL> cache;
+    int capacity;
+    int count;
+    DLL head, tail;
+
+    // THIS IS WHERE THE MAIN CODE STARTS
+
+    public LRUCache(int capacity) {
+        cache = new HashMap<Integer, DLL>();
+        this.capacity = capacity;
+        count = 0;
+
+        head = new DLL();
+        tail = new DLL();
+        head.pre = null;
+        tail.post = null;
+
+        head.post = tail;
+        tail.pre = head;
+    }
+    
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+            add(cache.get(key));
+            return cache.get(key).val;
+        }
+
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (!cache.containsKey(key)) {
+            DLL node = new DLL();
+            node.key = key;
+            node.val = value;
+            cache.put(key, node);
+            
+            if (count < capacity) {
+                count++;
+                add(node);
+            } else {
+                cache.remove(tail.pre.key);
+                tail.pre.pre.post = tail;
+                tail.pre = tail.pre.pre;
+                add(node);
+            }
+        } else {
+            cache.get(key).val = value;
+            remove(cache.get(key));
+            add(cache.get(key));
+        }
+    }
+}
+
+/*
+
+MOTHER FUCKER WHAT THE FUCK IS WRONG WITH YOU. THE ONLY ERROR WAS IN LINE 168. ONE LINE. FUCK. FUCK YOU DUMB ASS. FUCK YOU! FUCKING IDIOT. FUCKKKKIING IDDDIOT!!!!
+
+*/
